1ad2ddc01687ea11c9320f5149fcac87b674847c
Syed Atique <syed.atique@eurekasolutions.co.uk>
2019-10-16 10:09:42 +0100

    New Branch created by Syed

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -18,22 +18,27 @@ class Dec2Hex
                 System.err.println("Argument" + args[0] + " must be an integer.");
                 System.exit(1);
             }
-        }
-
-    	char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-         int rem, num;
-	num = Arg1;
-	String hexadecimal=""; 
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
         
-        while(num != 0)
-        {
-	    rem=num%16;
-	    hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
+
+    	 char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+       int rem, num;
+       num = Arg1;
+       String hexadecimal=""; 
+       System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
+       while(num != 0)
+       {
+          rem=num%16;
+          hexadecimal= ch[rem] + hexadecimal;
+          num= num/16;
         }
         
         System.out.println("Hexadecimal representation is : " + hexadecimal);
 
     }
+    else
+    {
+      System.out.println("No Decimal Value Provided. Please provide a decimal value to convert.");
+    }
+  }
 }
